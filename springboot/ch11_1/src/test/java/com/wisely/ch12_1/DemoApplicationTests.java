e4c36b1af9269da5ec5b665056916a251daf22a3
kingzhe2011 <king_zhe@163.com>
2017-02-09 18:32:29 +0800

    # first commit

---
+++ springboot/ch11_1/src/test/java/com/wisely/ch12_1/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.wisely.ch12_1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.wisely.ch11_1.DemoApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+@WebAppConfiguration
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ springboot/ch9_1/src/test/java/com/wisely/ch9_1/Ch91ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.wisely.ch9_1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Ch91Application.class)
+@WebAppConfiguration
+public class Ch91ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
